e848ac14683d10c368ae3fa7ad6a62eb3a04740c
Tepan223 <stevanusgabrielbb34@gmail.com>
2026-05-15 15:36:39 +0700

    first commit

---
+++ app/template/template.js
@@ -0,0 +1,48 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import styles from './styles/sec1.module.css'
+
+export default function Sec1() {
+
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+
+    const checkScreen = () => {
+      setIsMobile(window.innerWidth <= 1024)
+    }
+
+    checkScreen()
+
+    window.addEventListener("resize", checkScreen)
+
+    return () => {
+      window.removeEventListener("resize", checkScreen)
+    }
+
+  }, [])
+
+  return (
+    <div className={styles.sec1}>
+
+      {isMobile ? (
+        <div className={styles.sec1_mb}>
+          <div className={styles.content_container}>
+            <div className={styles.center_container}>
+              <div>awdawd</div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.sec1_pc}>
+          <div className={styles.content_container}>
+            <div className={styles.center_container}>
+            </div>
+          </div>
+        </div>
+      )}
+
+    </div>
+  )
+}
